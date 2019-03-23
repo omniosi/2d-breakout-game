3593ebb4bee9fbcce35db0a60783f5c0847710db
Ornette Coleman <omniosi@gmail.com>
2019-03-23 16:52:03 -0700

    split files added

---
+++ script.js
@@ -0,0 +1,33 @@
+// Tutorial source = https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+function draw() {
+  // clear the previous art
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //drawing code
+  drawBall();
+  x += dx;
+  y += dy;
+  if (x + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height || y + dy < 0) {
+    dy = -dy;
+  }
+}
+setInterval(draw, 10);
