0cf47dbad1d012cc00ab068d3c004556f8f2bf44
Ornette Coleman <omniosi@gmail.com>
2019-04-14 23:32:15 -0700

    giving the player some lives

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var score = 0;
+var lives = 3;
 
 var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
@@ -97,6 +98,11 @@ function drawScore() {
   ctx.fillStyle = "#0095DD";
   ctx.fillText("Score: " + score, 8, 20);
 }
+function drawLives() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -141,6 +147,7 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  drawLives();
   drawScore();
   collisionDetection();
 
@@ -156,9 +163,18 @@ function draw() {
       // dy += 2; // make the ball go faster
       dy = -dy;
     } else {
-      alert("GAME OVER");
-      document.location.reload();
-      clearInterval(interval); // Needed for Chrome to end game
+      lives--;
+      if (!lives) {
+        alert("GAME OVER");
+        document.location.reload();
+        clearInterval(interval); // Needed for Chrome to end game
+      } else {
+        x = canvas.width / 2;
+        y = canvas.height - 30;
+        dx = 2;
+        dy = -2;
+        paddleX = (canvas.width - paddleWidth) / 2;
+      }
     }
   }
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
